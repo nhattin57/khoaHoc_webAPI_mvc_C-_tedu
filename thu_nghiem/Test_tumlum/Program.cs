ecdee8de31b5b5abb6cc69eac2e5078518a696e0
nhattin57 <nhattin57@gmail.com>
2022-08-30 14:12:09 +0700

    KhoaHocTeDu

---
+++ thu_nghiem/Test_tumlum/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test_tumlum
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
